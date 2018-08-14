8aa48e139431d28fa04306169e268934d2582c97
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-14 23:39:37 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,32 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params(:name))
+    redirect_to genre_path(@artist)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@artist)
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
