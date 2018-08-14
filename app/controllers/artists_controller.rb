9c93d9d5512a834023eac3e34e0b93c028ed882b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-14 23:36:46 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -15,4 +15,18 @@ class ArtistsController < ApplicationController
     @artist = Artist.create(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
