94fca98694a03fe28796b033e44b8cc3efe907cc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-14 23:34:53 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,18 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
